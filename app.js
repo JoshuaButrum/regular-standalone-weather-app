7b5fc71cc05da2337dd005aa65de632d4945b374
ButrumJ <143197017+JoshuaButrum@users.noreply.github.com>
2025-01-29 12:09:59 -0600

    Modified app.js some more

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require('express')
+const app = express()
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const port = process.env.PORT || 3000
